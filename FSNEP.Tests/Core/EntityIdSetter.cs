4fb63ae20b7cc96a39f7c30aa979fb30450c23d9
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:35:02 +0000

    Updated the entityIdSetter so it is now strongly typed and has better comments. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4966. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6877

--- FSNEP.Tests/Core/EntityIdSetter.cs
+++ FSNEP.Tests/Core/EntityIdSetter.cs
@@ -5,30 +5,28 @@ using CAESArch.Core.Utils;
 namespace FSNEP.Tests.Core
 {
     /// <summary>
-    /// For better data integrity, it is imperitive that the <see cref="Entity.Id"/>
+    /// For better data integrity, it is imperitive that the DomainObject<T,IdT>.id
     /// property is read-only and set only by the ORM.  With that said, some unit tests need 
     /// Id set to a particular value; therefore, this utility enables that ability.  This class should 
-    /// never be used outside of the testing project; instead, implement <see cref="IHasAssignedId{IdT}" /> to 
-    /// expose a public setter.
+    /// never be used outside of the testing project.
     /// </summary>
     public static class EntityIdSetter
     {
         /// <summary>
-        /// Uses reflection to set the Id of a <see cref="EntityWithTypedId{IdT}" />.
+        /// Uses reflection to set the Id of a DomainObject
         /// </summary>
-        public static void SetIdOf<IdT>(object entity, IdT id)
+        public static void SetIdOf<T,IdT>(DomainObject<T,IdT> entity, IdT id)
         {
             // Set the data property reflectively
             var idProperty = entity.GetType().GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
-
-
+            
             Check.Ensure(idProperty != null, "idProperty could not be found");
 
             idProperty.SetValue(entity, id);
         }
 
         /// <summary>
-        /// Uses reflection to set the Id of a <see cref="EntityWithTypedId{IdT}" />.
+        /// Extension method that uses reflection to set the Id of a DomainObject
         /// </summary>
         public static object SetIdTo<T,IdT>(this DomainObject<T,IdT> entity, IdT id)
         {
