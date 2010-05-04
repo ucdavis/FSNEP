aa3b2789ec0e10bbcb843340194011e4a80125c1
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:33:24 +0000

    Added in some base users. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4729. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6755

--- FSNEP.Tests/Core/RepositoryTestBase.cs
+++ FSNEP.Tests/Core/RepositoryTestBase.cs
@@ -1,11 +1,22 @@
+using System;
+using System.Collections.Generic;
 using CAESArch.BLL;
+using FSNEP.BLL.Impl;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NHibernate.Cfg;
+using FSNEP.Core.Domain;
 
 namespace FSNEP.Tests.Core
 {
     public abstract class RepositoryTestBase
     {
+        public List<Guid> UserIds { get; set; }
+
+        protected RepositoryTestBase()
+        {
+            UserIds = new List<Guid>();
+        }
+
         [TestInitialize]
         public void CreateDB()
         {
@@ -22,8 +33,54 @@ namespace FSNEP.Tests.Core
             using (var ts = new TransactionScope())
             {
                 //load base data
+                CreateUsers();
 
                 ts.CommitTransaction();
+            }
+        }
+
+        private void CreateUsers()
+        {
+            var userId = Guid.NewGuid();
+            
+            var userBLL = new UserBLL(null);
+
+            var supervisor = new User
+                                 {
+                                     FirstName = "FNameS",
+                                     LastName = "LNameS",
+                                     Salary = 10,
+                                     FTE = 1,
+                                     IsActive = true
+                                 };
+
+            supervisor.Supervisor = supervisor; //i'm my own boss
+            supervisor.SetUserID(userId);
+
+            UserIds.Add(userId);
+
+            userBLL.Repository.EnsurePersistent(supervisor, true);
+
+            //Create 5 users
+            for (int i = 0; i < 5; i++)
+            {
+                var user = new User
+                               {
+                                   FirstName = "FName" + i,
+                                   LastName = "LName" + i,
+                                   Salary = i + 1,
+                                   FTE = 1,
+                                   IsActive = true,
+                                   Supervisor = supervisor
+                               };
+
+                userId = Guid.NewGuid();
+
+                user.SetUserID(userId);
+
+                UserIds.Add(userId);
+
+                userBLL.Repository.EnsurePersistent(user, true);
             }
         }
     }
