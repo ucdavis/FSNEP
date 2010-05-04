d5ebb1fda6d7ddf1323e136e1703d74c385b3625
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:35:38 +0000

    Simplified extension (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5031. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6926

--- FSNEP.Tests/Core/Extensions/AssertExtensions.cs
+++ FSNEP.Tests/Core/Extensions/AssertExtensions.cs
@@ -24,7 +24,6 @@ namespace FSNEP.Tests.Core.Extensions
         public static void AssertErrorsAre(this ModelStateDictionary modelState, params string[] errors)
         {
             var resultsList = new List<string>();
-
             foreach (var result in modelState.Values)
             {
                 foreach (var errs in result.Errors)
@@ -34,24 +33,10 @@ namespace FSNEP.Tests.Core.Extensions
             }
 
             Assert.AreEqual(resultsList.Count, errors.Length, "Number of error messages do not match");
-            //foreach (var error in errors)
-            //{
-            //    Assert.IsTrue(resultsList.Contains(error), "Expected error \"" + error + "\" not found");
-            //}
-            //newUserModel.ViewData.ModelState.AssertErrorsAre("FirstName: The value cannot be null.",
-            //                                                 "FirstName: The value cannot be null.");
-            //Changed it so the above cut and paste error with a unit test could not happen.
-            var errorsList = new List<string>();
-            foreach (var error  in errors)
+            foreach (var error in errors)
             {
-                errorsList.Add(error);
-            }
-
-            //Look through the errors in the modelState and check they are as expected.
-            foreach (var error in resultsList)
-            {
-                Assert.IsTrue(errorsList.Contains(error), "Expected error \"" + error + "\" not found");
-            }
+                Assert.IsTrue(resultsList.Contains(error), "Expected error \"" + error + "\" not found");
+            }            
         }
     }
 }
