278a5b23c34bb484a2d1e92827cd022401f9fa5e
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:39 +0000

    Added Repositiry test for ExpenseType (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4933. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6847

---
+++ FSNEP.Tests/Repositories/ExpenseTypeRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using CAESArch.BLL;
+using CAESArch.BLL.Repositories;
+using CAESArch.Core.DataInterfaces;
+using FSNEP.Core.Domain;
+using FSNEP.Tests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FSNEP.Tests.Repositories
+{
+    [TestClass]
+    public class ExpenseTypeRepositoryTests : RepositoryTestBase 
+    {
+        private readonly IRepository<ExpenseType> _expenseTypeRepository = new Repository<ExpenseType>();
+
+        //Don't load any data
+        protected override void LoadData() { }
+
+        [TestMethod]
+        public void CanSaveCompleteAndValidExpenseType()
+        {
+            var expenseType = new ExpenseType { Name = "123456789 123456789 123456789 123456789 1234567890" };
+
+            using (var ts = new TransactionScope())
+            {
+                _expenseTypeRepository.EnsurePersistent(expenseType);
+
+                ts.CommitTransaction();
+            }
+
+            Assert.AreEqual(false, expenseType.IsTransient());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void ExpenseTypeDoesNotSaveWithNullName()
+        {
+            var expenseType = new ExpenseType { Name = null };
+
+            try
+            {
+                using (var ts = new TransactionScope())
+                {
+                    _expenseTypeRepository.EnsurePersistent(expenseType);
+
+                    ts.CommitTransaction();
+                }
+            }
+            catch (Exception message)
+            {
+                Assert.AreEqual("Object of type FSNEP.Core.Domain.ExpenseType could not be persisted\n\n\r\nValidation Errors: Name, The value cannot be null.\r\nName, The length of the value must fall within the range \"0\" (Ignore) - \"50\" (Inclusive).\r\n", message.Message, "Expected Exception Not encountered");
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void ExpenseTypeDoesNotSaveWithNameGreaterThan50Characters()
+        {
+            var expenseType = new ExpenseType { Name = "123456789 123456789 123456789 123456789 123456789 1" };
+
+            try
+            {
+                using (var ts = new TransactionScope())
+                {
+                    _expenseTypeRepository.EnsurePersistent(expenseType);
+
+                    ts.CommitTransaction();
+                }
+            }
+            catch (Exception message)
+            {
+                Assert.AreEqual("Object of type FSNEP.Core.Domain.ExpenseType could not be persisted\n\n\r\nValidation Errors: Name, The length of the value must fall within the range \"0\" (Ignore) - \"50\" (Inclusive).\r\n", message.Message, "Expected Exception Not encountered");
+                throw;
+            }
+        }
+    }
+}
