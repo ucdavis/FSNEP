e46f11c1db71d8fec7acb0cb5af18f374969d3be
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:41:12 +0000

    Created two blank SPROCs which will handle reporting for the Cost Share report (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6003. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7331

---
+++ FSNEP.SQL/Reporting/usp_GenerateExpenseCostShare.sql
@@ -0,0 +1,26 @@
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'usp_GenerateExpenseCostShare')
+	BEGIN
+		DROP  Procedure  usp_GenerateExpenseCostShare
+	END
+
+GO
+
+CREATE Procedure usp_GenerateExpenseCostShare
+/*
+	(
+		@parameter1 int = 5,
+		@parameter2 datatype OUTPUT
+	)
+
+*/
+AS
+
+
+GO
+
+/*
+GRANT EXEC ON usp_GenerateExpenseCostShare TO PUBLIC
+
+GO
+*/
+
---
+++ FSNEP.SQL/Reporting/usp_GenerateTimeCostShare.sql
@@ -0,0 +1,26 @@
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'usp_GenerateTimeCostShare')
+	BEGIN
+		DROP  Procedure  usp_GenerateTimeCostShare
+	END
+
+GO
+
+CREATE Procedure usp_GenerateTimeCostShare
+/*
+	(
+		@parameter1 int = 5,
+		@parameter2 datatype OUTPUT
+	)
+
+*/
+AS
+
+
+GO
+
+/*
+GRANT EXEC ON usp_GenerateTimeCostShare TO PUBLIC
+
+GO
+*/
+
