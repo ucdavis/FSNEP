263011b7b4f54e8117a2ccb8590e804533ce152a
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:40:50 +0000

    Added  a script which can insert the values for the two required lookup tables, Status and AuditActionTypes (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5920. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7303

---
+++ FSNEP.SQL/Scheema/InsertLookupData.sql
@@ -0,0 +1,12 @@
+﻿
+SET IDENTITY_INSERT [dbo].[Status] ON
+INSERT [dbo].[Status] ([ID], [Name]) VALUES (1, N'Current')
+INSERT [dbo].[Status] ([ID], [Name]) VALUES (2, N'PendingReview')
+INSERT [dbo].[Status] ([ID], [Name]) VALUES (3, N'Approved')
+INSERT [dbo].[Status] ([ID], [Name]) VALUES (4, N'Disapproved')
+SET IDENTITY_INSERT [dbo].[Status] OFF
+
+INSERT [dbo].[AuditActionTypes] ([ID], [ActionCodeName]) VALUES (N'C', N'Create')
+INSERT [dbo].[AuditActionTypes] ([ID], [ActionCodeName]) VALUES (N'D', N'Delete')
+INSERT [dbo].[AuditActionTypes] ([ID], [ActionCodeName]) VALUES (N'U', N'Update')
+ 
