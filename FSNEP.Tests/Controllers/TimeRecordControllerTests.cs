8ff01c5f782911eebe8e4558a3f7b172ff5ca218
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:37:17 +0000

    User Tests WIP (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5203. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7042

--- FSNEP.Tests/Controllers/TimeRecordControllerTests.cs
+++ FSNEP.Tests/Controllers/TimeRecordControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Web;
 using System.Web.Mvc;
 using FSNEP.BLL.Impl;
 using FSNEP.BLL.Interfaces;
@@ -17,21 +19,31 @@ using FSNEP.Tests.Core.Extensions;
 using UCDArch.Testing;
 using Rhino.Mocks.Interfaces;
 
+
 namespace FSNEP.Tests.Controllers
 {
     [TestClass]
     public class TimeRecordControllerTests : Core.ControllerTestBase<TimeRecordController>
     {
+        private User _user = CreateValidUser();
+        /// <summary>
+        /// Override this method to setup a controller that doesn't have an empty ctor
+        /// </summary>
         protected override void SetupController()
         {
             var timeRecordBll = MockRepository.GenerateStub<ITimeRecordBLL>();
             var userBll = MockRepository.GenerateStub<IUserBLL>();
-            var timeRecordCalendarGenerator = MockRepository.GenerateStub<ITimeRecordCalendarGenerator>();
+            //var timeRecordCalendarGenerator = MockRepository.GenerateStub<ITimeRecordCalendarGenerator>();            
+            var timeRecordCalendarGenerator = new TimeRecordCalendarGenerator();
+            FakeTimeRecord(timeRecordBll);
 
             CreateController(timeRecordBll, userBll, timeRecordCalendarGenerator);
         }
 
         #region Routing maps
+        /// <summary>
+        /// Routings the time record entry maps entry.
+        /// </summary>
         [TestMethod]
         public void RoutingTimeRecordEntryMapsEntry()
         {
@@ -39,7 +51,196 @@ namespace FSNEP.Tests.Controllers
             "~/TimeRecord/TimeRecordEntry/5"
                 .ShouldMapTo<TimeRecordController>(a => a.TimeRecordEntry(id));
         }
-
         #endregion Routing maps
+
+        #region AddEntry Tests
+
+        /// <summary>
+        /// Determines whether this instance [can add valid time record entry].
+        /// </summary>
+        [TestMethod, Ignore]
+        public void CanAddValidTimeRecordEntry()
+        {
+            var timeRecordEntry = new TimeRecordEntry();
+            /*
+            var mockIdentity = new MockIdentity();
+            var mockUser = _user;
+            //Controller.ControllerContext.HttpContext.Expect(u => u.User.Identity).Return(mockIdentity);
+            Controller.ControllerContext.HttpContext.Expect(u=>u.User).Return()
+            Controller.ControllerContext.HttpContext.User.Expect(a => a.Identity).Return(mockIdentity);
+
+            //Controller.ControllerContext.HttpContext.Expect(a => a.User).Return(mockIdentity);
+            */
+
+            //var mockHttpContext = new MockHttpContext();            
+            //Controller.ControllerContext.Expect(a => a.HttpContext).Return(mockHttpContext).Repeat.Any();
+
+            //IPrincipal userPrincipal = new MockPrincipal();
+            //Controller.ControllerContext.HttpContext.Expect(a => a.User).Return(userPrincipal).Repeat.Any();
+
+            
+            
+
+
+            var result = Controller.AddEntry(1, CreateValidTimeRecordEntry());
+        }
+
+        #endregion AddEntry Tests
+
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Fakes the time record.
+        /// </summary>
+        /// <param name="bll">The BLL.</param>
+        private void FakeTimeRecord(ITimeRecordBLL timeRecordBLL)
+        {
+            TimeRecord timeRecord = CreateValidTimeRecord();
+            timeRecord.SetIdTo(1);
+
+            timeRecordBLL.Expect(a => a.GetNullableByID(1)).Return(timeRecord).Repeat.Any();
+            
+        }
+
+        /// <summary>
+        /// Creates the valid time record.
+        /// </summary>
+        /// <returns></returns>
+        private TimeRecord CreateValidTimeRecord()
+        {
+            return new TimeRecord
+                       {
+                           Month = DateTime.Now.Month,
+                           Year = DateTime.Now.Year,
+                           Salary = 200,
+                           Status = new Status { Name = "S1" },
+                           User = _user,
+                           ReviewComment = "A review is a review except when it isn't."
+                       };
+        }
+
+        /// <summary>
+        /// Creates the valid user.
+        /// </summary>
+        /// <returns></returns>
+        private static User CreateValidUser()
+        {
+            var user = new User
+            {
+                FirstName = "FName",
+                LastName = "LName",
+                Salary = 1,
+                BenefitRate = 2,
+                FTE = 1,
+                IsActive = true,
+                UserName = "UserName"
+            };
+            user.Supervisor = user; //I'm my own supervisor
+            user.Projects = new List<Project> {new Project {Name = "Project1"}};
+            user.FundTypes = new List<FundType>{new FundType{Name = "FundType1"}};
+
+            var userId = Guid.NewGuid();
+            user.SetUserID(userId);
+
+            return user;
+        }
+
+        /// <summary>
+        /// Creates the valid time record entry.
+        /// </summary>
+        /// <returns></returns>
+        private TimeRecordEntry CreateValidTimeRecordEntry()
+        {
+            const int validDate = 25;
+            const string validComment = "Comment";
+            const double validHours = 6.5;
+            //TODO: Verify Record, FundType, Project, and Account values.
+            return new TimeRecordEntry
+            {
+                Date = validDate,
+                Hours = validHours,
+                Comment = validComment,
+                Record = new Record(),
+                FundType = new FundType(),
+                Project = new Project(),
+                Account = new Account()
+            };
+        }
+
+        #endregion Helper Methods
+
+        #region mocks
+        public class MockIdentity : IIdentity
+        {
+            public string AuthenticationType
+            {
+                get
+                {
+                    return "MockAuthentication";
+                }
+            }
+
+            public bool IsAuthenticated
+            {
+                get
+                {
+                    return true;
+                }
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return "UserName";
+                }
+            }
+        }
+        
+
+        public class MockPrincipal : IPrincipal
+        {
+            IIdentity identity;
+
+            public IIdentity Identity
+            {
+                get
+                {
+                    if (identity == null)
+                    {
+                        identity = new MockIdentity();
+                    }
+                    return identity;
+                }
+            }
+
+            public bool IsInRole(string role)
+            {
+                return false;
+            }
+        }
+
+        public class MockHttpContext : HttpContextBase
+        {
+            private IPrincipal user;
+
+            public override IPrincipal User
+            {
+                get
+                {
+                    if (user == null)
+                    {
+                        user = new MockPrincipal();
+                    }
+                    return user;
+                }
+                set
+                {
+                    user = value;
+                }
+            }
+        }
+        #endregion
     }
 }
