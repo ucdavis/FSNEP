ea2594ddf1925e9d834055c7992a6501af63e946
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:41:05 +0000

    Notify users now completely setup to send emails to the 1.0 fte time record users on the first of april and october... The method will only execute on those days, even if called at another time (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5975. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7322

--- FSNEP.SQL/SPROCs/NotifyUsersOfSemiAnnualCertification.sql
+++ FSNEP.SQL/SPROCs/NotifyUsersOfSemiAnnualCertification.sql
@@ -6,23 +6,42 @@ DECLARE @monthInt int
 SET @dayInt = (SELECT DATEPART(DAY, GETDATE()))
 SET @monthInt = (SELECT DATEPART(month, GETDATE()))
 
-IF @dayInt = 7 --Only continue if it is the 1st of the month
+IF (@dayInt = 13) AND (@monthInt = 4 OR @monthInt = 1)
 BEGIN
 
-	--Only continue if it is april or october
-	IF @monthInt = 4 OR @monthInt = 10
-	BEGIN
+	DECLARE @MailList CURSOR
+	SET @MailList = CURSOR FOR
 
-		SELECT @dayInt, @monthInt
+	--Get all of the 1.0 FTE TimeSheet Users
+	SELECT     aspnet_Membership.Email, Users.FirstName + ' ' + Users.LastName as FullName
+	FROM         aspnet_Membership INNER JOIN
+						  aspnet_UsersInRoles ON aspnet_Membership.UserId = aspnet_UsersInRoles.UserId INNER JOIN
+						  aspnet_Roles ON aspnet_UsersInRoles.RoleId = aspnet_Roles.RoleId AND aspnet_UsersInRoles.RoleId = aspnet_Roles.RoleId INNER JOIN
+						  Users ON aspnet_Membership.UserId = Users.UserId
+	WHERE     (aspnet_Roles.RoleName = N'Timesheet User') AND (FTE = 1)
 
-		--Get all of the 1.0 FTE TimeSheet Users
-		SELECT     aspnet_Membership.Email, Users.FirstName, Users.LastName
-		FROM         aspnet_Membership INNER JOIN
-							  aspnet_UsersInRoles ON aspnet_Membership.UserId = aspnet_UsersInRoles.UserId INNER JOIN
-							  aspnet_Roles ON aspnet_UsersInRoles.RoleId = aspnet_Roles.RoleId AND aspnet_UsersInRoles.RoleId = aspnet_Roles.RoleId INNER JOIN
-							  Users ON aspnet_Membership.UserId = Users.UserId
-		WHERE     (aspnet_Roles.RoleName = N'Timesheet User') AND (FTE = 1)
+	OPEN @MailList
 
-		--Send them all emails
-	END
-END 
+	DECLARE @Email varchar(50), @FullName varchar(100)
+	FETCH NEXT FROM @MailList INTO @Email, @FullName
+
+	WHILE (@@FETCH_STATUS = 0)
+		BEGIN
+			--Send emails to each matching user
+			DECLARE @bodyText varchar(MAX)
+
+			SET @bodyText = 'Dear ' + @FullName + '. This is the body of the test message.
+			Database Mail Received By you Successfully.'
+			
+			EXEC msdb.dbo.sp_send_dbmail
+			@recipients=@Email
+			@subject='Test message',
+			@body=@bodyText
+			
+			FETCH NEXT FROM @MailList INTO @Email, @FullName
+		END	
+
+	CLOSE @MailList
+	DEALLOCATE @MailList
+
+END
