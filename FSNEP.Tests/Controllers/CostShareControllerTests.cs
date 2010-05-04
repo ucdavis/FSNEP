d92b47e04fffdc429e9f847547e85c1acf5e6973
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:38:40 +0000

    Code cleanup. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5472. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7141

--- FSNEP.Tests/Controllers/CostShareControllerTests.cs
+++ FSNEP.Tests/Controllers/CostShareControllerTests.cs
@@ -36,7 +36,7 @@ namespace FSNEP.Tests.Controllers
         #region Routing Tests
 
         /// <summary>
-        /// Routings the cost share history maps to history.
+        /// Routing cost share history maps to history.
         /// </summary>
         [TestMethod]
         public void RoutingCostShareHistoryMapsToHistory()
@@ -44,7 +44,67 @@ namespace FSNEP.Tests.Controllers
             "~/CostShare/History".ShouldMapTo<CostShareController>(a => a.History());
         }
 
+        /// <summary>
+        /// Routing cost share current maps to current.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareCurrentMapsToCurrent()
+        {
+            "~/CostShare/Current".ShouldMapTo<CostShareController>(a => a.Current());
+        }
+
+        /// <summary>
+        /// Routing cost share review maps to review.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareReviewMapsToReview()
+        {
+            const int id = 5;
+            "~/CostShare/Review/5".ShouldMapTo<CostShareController>(a => a.Review(id));
+        }
+
+        /// <summary>
+        /// Routing cost share entry maps to entry.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareEntryMapsToEntry()
+        {
+            const int id = 5;
+            "~/CostShare/Entry/5".ShouldMapTo<CostShareController>(a => a.Entry(id));
+        }
+
+        /// <summary>
+        /// Routing cost share entry with parameters maps to entry.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareEntryWithParametersMapsToEntry()
+        {
+            "~/CostShare/Entry".ShouldMapTo<CostShareController>(a => a.Entry(5, new CostShareEntry()), true);
+        }
+
+        /// <summary>
+        /// Routing cost share remove entry maps to remove entry.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareRemoveEntryMapsToRemoveEntry()
+        {
+            const int id = 5;
+            "~/CostShare/RemoveEntry/5".ShouldMapTo<CostShareController>(a => a.RemoveEntry(id), true);
+        }
+
+        /// <summary>
+        /// Routing cost share submit maps to submit.
+        /// </summary>
+        [TestMethod]
+        public void RoutingCostShareSubmitMapsToSubmit()
+        {
+            const int id = 5;
+            "~/CostShare/Submit/5".ShouldMapTo<CostShareController>(a => a.Submit(id));
+        }
+        
+
         #endregion Routing Tests
+
 
     }
 }
