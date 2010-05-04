92cc0d37ff3d2899f705f90992ab27069746ea2a
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:52 +0000

    Added ActivityType Tests. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4949. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6861

--- FSNEP.Tests/Controllers/LookupControllerTests.cs
+++ FSNEP.Tests/Controllers/LookupControllerTests.cs
@@ -6,12 +6,18 @@ using MvcContrib.TestHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhino.Mocks;
 using FSNEP.Core.Domain;
+using FSNEP.Tests.Core.Extensions;
 
 namespace FSNEP.Tests.Controllers
 {
     [TestClass]
     public class LookupControllerTests : ControllerTestBase<LookupController>
     {
+        /// <summary>
+        /// 51 characters for Name
+        /// </summary>
+        public const string InvalidValueName = "123456789 123456789 123456789 123456789 123456789 1";
+
         [TestMethod]
         public void CreateProjectSavesNewProject()
         {
@@ -45,7 +51,7 @@ namespace FSNEP.Tests.Controllers
             Controller.CreateProject(newProject);
 
             projectRepository
-                .AssertWasNotCalled(a => a.EnsurePersistent(newProject));//make sure we called persist
+                .AssertWasNotCalled(a => a.EnsurePersistent(newProject));//make sure we didn't call persist
         }
 
         [TestMethod]
@@ -108,9 +114,9 @@ namespace FSNEP.Tests.Controllers
         [TestMethod]
         public void ProjectsGetsOnlyActiveProjects()
         {
-            //4 projects, 2 are active
+            //5 projects, 3 are active
             var projects =
-                new[] {new Project {IsActive = true}, new Project {IsActive = true}, new Project(), new Project()}.
+                new[] { new Project { IsActive = true }, new Project { IsActive = true }, new Project { IsActive = true }, new Project(), new Project() }.
                     AsQueryable();
 
             var projectRepository = FakeRepository<Project>();
@@ -122,7 +128,7 @@ namespace FSNEP.Tests.Controllers
                 .AssertViewRendered()
                 .WithViewData<List<Project>>();
 
-            Assert.AreEqual(2, result.Count, "Should only get the two active projects");
+            Assert.AreEqual(3, result.Count, "Should only get the three active projects");
         }
 
         [TestMethod]
@@ -155,13 +161,9 @@ namespace FSNEP.Tests.Controllers
             const string validIndicator = "12";
             const int validCategoryId = 1;
 
-            //CreateActivityType calls GetNullableById for ActivityCategory
-            var validActivityCategory = new ActivityCategory();
-            var validActivityCategoryRepository = FakeRepository<ActivityCategory>();
-            validActivityCategoryRepository.Expect(a => a.GetNullableByID(validCategoryId)).Return(validActivityCategory);
-            Controller.Repository.Expect(a => a.OfType<ActivityCategory>()).Return(validActivityCategoryRepository).Repeat.Any();
+            FakeActivityCategory(validCategoryId);            
 
-            var newActivityType = new ActivityType() { Name = "ValidActivityType", Indicator = validIndicator};
+            var newActivityType = new ActivityType { Name = "ValidActivityType", Indicator = validIndicator};
 
             var activityTypeRepository = FakeRepository<ActivityType>();
             activityTypeRepository
@@ -180,7 +182,87 @@ namespace FSNEP.Tests.Controllers
         }
 
         /// <summary>
-        /// Inactivate ActivityType Redirects On Valid ActivityTypeId
+        /// Create ActivityType Does Not Save ActivityType With Long Name
+        /// </summary>
+        [TestMethod]
+        public void CreateActivityTypeDoesNotSaveActivityTypeWithLongName()
+        {
+            const string validIndicator = "12";
+            const int validCategoryId = 1;
+            
+            FakeActivityCategory(validCategoryId);            
+
+            var newActivityType = new ActivityType { 
+                Name = InvalidValueName, 
+                Indicator = validIndicator };
+            
+            var activityTypeRepository = FakeRepository<ActivityType>();
+            activityTypeRepository
+                .Expect(a => a.EnsurePersistent(Arg<ActivityType>.Is.Anything))
+                .WhenCalled(a => newActivityType = (ActivityType)a.Arguments.First()); //set newActivityType to the activityType that was saved
+
+            Controller.Repository.Expect(a => a.OfType<ActivityType>()).Return(activityTypeRepository);
+
+            Controller.CreateActivityType(newActivityType, validCategoryId);
+
+            activityTypeRepository
+                .AssertWasNotCalled(a => a.EnsurePersistent(newActivityType));//make sure we didn't call persist
+
+        }
+
+        [TestMethod]
+        public void CreateActivityTypeRedirectsActivityType()
+        {
+            const int validCategoryId = 1;
+
+            FakeActivityCategory(validCategoryId);
+
+            Controller.Repository.Expect(a => a.OfType<ActivityType>()).Return(FakeRepository<ActivityType>());
+
+            Controller.CreateActivityType(new ActivityType(), validCategoryId)
+                .AssertActionRedirect()
+                .ToAction<LookupController>(a => a.ActivityTypes());
+        }
+
+        /// <summary>
+        /// Inactivate ActivityType Redirects On Invalid ActivityType Id
+        /// </summary>
+        [TestMethod]
+        public void InactivateActivityTypeRedirectsOnInvalidActivityTypeId()
+        {
+            const int invalidActivityTypeId = 42;
+
+            var activityTypeRepository = FakeRepository<ActivityType>();
+            activityTypeRepository.Expect(a => a.GetNullableByID(invalidActivityTypeId)).Return(null);
+
+            Controller.Repository.Expect(a => a.OfType<ActivityType>()).Return(activityTypeRepository);
+
+            Controller.InactivateActivityType(invalidActivityTypeId)
+                .AssertActionRedirect()
+                .ToAction<LookupController>(a => a.ActivityTypes());
+        }
+
+        /// <summary>
+        /// Inactivate ActivityType Persists Changes On Valid ActivityType Id
+        /// </summary>
+        [TestMethod]
+        public void InactivateActivityTypePersistsChangesOnValidActivityTypeId()
+        {
+            var activeActivityType = new ActivityType { IsActive = true };
+
+            var activityTypeRepository = FakeRepository<ActivityType>();
+            activityTypeRepository.Expect(a => a.GetNullableByID(1)).IgnoreArguments().Return(activeActivityType);
+
+            Controller.Repository.Expect(a => a.OfType<ActivityType>()).Return(activityTypeRepository).Repeat.Any();
+
+            Controller.InactivateActivityType(activeActivityType.ID);
+
+            Assert.AreEqual(false, activeActivityType.IsActive, "ActivityType should have been inactivated");
+            activityTypeRepository.AssertWasCalled(a => a.EnsurePersistent(activeActivityType), a => a.Repeat.Once()); //Make sure we saved the change
+        }
+        
+        /// <summary>
+        /// Inactivate ActivityType Redirects On Valid ActivityType Id
         /// </summary>
         [TestMethod]
         public void InactivateActivityTypeRedirectsOnValidActivityTypeId()
@@ -196,6 +278,112 @@ namespace FSNEP.Tests.Controllers
             Controller.InactivateActivityType(validActivityTypeId)
                 .AssertActionRedirect()
                 .ToAction<LookupController>(a => a.ActivityTypes());
+        }
+
+        /// <summary>
+        /// ActivityType Gets Only Active ActivityTypes
+        /// Internally, this needs the CategoryRepository Mocked for Queryable
+        /// </summary>
+        [TestMethod]
+        public void ActivityTypeGetsOnlyActiveActivityTypes()
+        {           
+            //5 ActivityTypes, 3 are active
+            var activityTypes =
+                new[]
+                    {
+                        new ActivityType { Name = "Valid", IsActive = true}, 
+                        new ActivityType { Name = "Valid", IsActive = true}, 
+                        new ActivityType { Name = "Valid", IsActive = true},
+                        new ActivityType { Name = "Valid"}, 
+                        new ActivityType { Name = "Valid"}
+                    }.AsQueryable();
+
+            var activityTypeRepository = FakeRepository<ActivityType>();
+            activityTypeRepository.Expect(a => a.Queryable).Return(activityTypes);
+
+            Controller.Repository.Expect(a => a.OfType<ActivityType>()).Return(activityTypeRepository);
+
+            FakeQueryableActivityCategory();            
+
+            var result = Controller.ActivityTypes()
+                .AssertViewRendered()
+                .WithViewData<List<ActivityType>>();
+
+            Assert.AreEqual(3, result.Count, "Should only get the three active ActivityTypes");
+        }
+
+        /// <summary>
+        /// Routing ActivityTypes Gets All ActivityTypes
+        /// </summary>
+        [TestMethod]
+        public void RoutingActivityTypesGetsAllActivityTypes()
+        {
+            "~/Administration/Lookups/ActivityTypes"
+                .ShouldMapTo<LookupController>(a => a.ActivityTypes());
+        }
+
+        /// <summary>
+        /// Routing InactivateActivityType Calls InactivateActivityType With Parameter
+        /// </summary>
+        [TestMethod]
+        public void RoutingInactivateActivityTypeCallsInactivateActivityTypeWithParameter()
+        {
+            "~/Administration/Lookups/InactivateActivityType/10"
+                .ShouldMapTo<LookupController>(a => a.InactivateActivityType(10));
+        }
+
+        /// <summary>
+        /// Routing Create ActivityType Calls Create ActivityType
+        /// </summary>
+        [TestMethod]
+        public void RoutingCreateActivityTypeCallsCreateActivityType()
+        {
+            //TODO: When we get the next version of MvcContrib, check if .ShouldMapTo works for this test
+            "~/Administration/Lookups/CreateActivityType".ShouldMapToIgnoringParams("Lookup", "CreateActivityType");
+
+            /*
+            var routeData = "~/Administration/Lookups/CreateActivityType".Route();
+
+            Assert.AreEqual("CreateActivityType", routeData.Values["action"]);
+            Assert.AreEqual("Lookup", routeData.Values["controller"]);
+            */
+
+            /*
+            const int activityCategory = 1;
+
+            "~/Administration/Lookups/CreateActivityType"
+                .ShouldMapTo<LookupController>(a => a.CreateActivityType(null,activityCategory));
+            */
+        }
+
+        /// <summary>
+        /// Fake a Queryable ActivityCategoryRepository.
+        /// </summary>
+        private void FakeQueryableActivityCategory()
+        {
+            var validActivityCategories =
+                new[]
+                    {
+                        new ActivityCategory {IsActive = true, Name = "ValidCategory"},
+                        new ActivityCategory {IsActive = true, Name = "ValidCategory"}
+                    }.AsQueryable();
+            var validActivityCategoryRepository = FakeRepository<ActivityCategory>();
+            validActivityCategoryRepository.Expect(a => a.Queryable).Return(validActivityCategories);
+            Controller.Repository.Expect(a => a.OfType<ActivityCategory>()).Return(validActivityCategoryRepository).Repeat.Any();
+        }
+
+        /// <summary>
+        /// For ActivityType, fake the ActivityCategory
+        /// </summary>
+        /// <param name="validCategoryId"></param>
+        private void FakeActivityCategory(int validCategoryId)
+        {
+            //CreateActivityType calls GetNullableById for ActivityCategory
+            var validActivityCategory = new ActivityCategory{Name = "ValidCategory"};
+            var validActivityCategoryRepository = FakeRepository<ActivityCategory>();
+            validActivityCategoryRepository.Expect(a => a.GetNullableByID(validCategoryId)).Return(validActivityCategory);
+            Controller.Repository.Expect(a => a.OfType<ActivityCategory>()).Return(validActivityCategoryRepository).Repeat.Any();
+           
         }
     }
 }
