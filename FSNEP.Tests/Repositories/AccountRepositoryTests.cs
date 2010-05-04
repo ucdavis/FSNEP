ccd83acb9a780d223b5f5688e328603ad505d9c0
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:45 +0000

    Added test for IndirectCostPercent (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4937. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6851

--- FSNEP.Tests/Repositories/AccountRepositoryTests.cs
+++ FSNEP.Tests/Repositories/AccountRepositoryTests.cs
@@ -40,7 +40,7 @@ namespace FSNEP.Tests.Repositories
         [ExpectedException(typeof(ApplicationException))]
         public void AccountDoesNotSaveWithNullName()
         {
-            var accountCategory = new Account()
+            var accountCategory = new Account
               {
                   Name = null,
                   IndirectCost = 0.30
@@ -66,7 +66,7 @@ namespace FSNEP.Tests.Repositories
         [ExpectedException(typeof(ApplicationException))]
         public void AccountDoesNotSaveWithNameGreaterThan50Characters()
         {
-            var accountCategory = new Account()
+            var accountCategory = new Account
             {
                 Name = "123456789 123456789 123456789 123456789 123456789 1",
                 IndirectCost = 0.15
@@ -92,7 +92,7 @@ namespace FSNEP.Tests.Repositories
         [ExpectedException(typeof(ApplicationException))]
         public void AccountDoesNotSaveWithIndirectCostLessThanZero()
         {
-            var accountCategory = new Account()
+            var accountCategory = new Account
             {
                 Name = "123456789 123456789 123456789 123456789 1234567890",
                 IndirectCost = -0.15
@@ -138,6 +138,30 @@ namespace FSNEP.Tests.Repositories
                 Assert.AreEqual("Object of type FSNEP.Core.Domain.Account could not be persisted\n\n\r\nValidation Errors: IndirectCost, The value must fall within the range \"0\" (Inclusive) - \"0.3\" (Inclusive).\r\n", message.Message, "Expected Exception Not encountered");
                 throw;
             }
+        }
+
+        [TestMethod]
+        public void IndirectCostPercent()
+        {
+            var accountCategory = new Account
+            {
+                Name = "Test",
+                IndirectCost = 0.29
+            };
+            
+            //TODO:Review. Is the indirect cost percent a problem? Without the round, it returns 28.999999... Should it be a decimal?
+            Assert.AreEqual(29, Math.Round(accountCategory.IndirectCostPercent,2));
+            //Tostring rounds it correctly.
+            Assert.AreEqual("Test (29%)", accountCategory.ToString());
+
+            accountCategory.IndirectCost = .299;
+            Assert.AreEqual("Test (29.9%)", accountCategory.ToString());
+
+            accountCategory.IndirectCostPercent = 10;
+            Assert.AreEqual(0.10, accountCategory.IndirectCost);
+
+            accountCategory.IndirectCostPercent = 29.9;
+            Assert.AreEqual(0.299, accountCategory.IndirectCost);
         }
     }
 }
