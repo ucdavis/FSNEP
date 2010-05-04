71e2f5e98fcafe880cde7299742aaa1ca3ba5c07
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:41:17 +0000

    Added in a UDF which will show the hours during different periods, like Q1, Q2, etc (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6011. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7338

---
+++ FSNEP.SQL/Reporting/udf_QuarterTotalHours.sql
@@ -0,0 +1,45 @@
+﻿ IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[udf_QuarterTotalHours]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+	DROP FUNCTION [dbo].[udf_QuarterTotalHours]
+GO
+
+CREATE FUNCTION [dbo].[udf_QuarterTotalHours]
+(
+	-- Add the parameters for the function here
+	@year int,
+	@quarter char(2)
+)
+RETURNS int
+AS
+BEGIN
+
+-- Declare the return variable here
+	DECLARE @result int
+	
+	IF @quarter = 'Q1'
+			SELECT @result = SUM(HoursInMonth) 
+			FROM HoursInMonths 
+			WHERE [Year] = @year AND [Month] IN (10, 11, 12)
+	ELSE IF @quarter = 'Q2' 
+			SELECT @result = SUM(HoursInMonth) 
+			FROM HoursInMonths 
+			WHERE [Year] = @year AND [Month] IN (1, 2, 3)
+	ELSE IF @quarter = 'Q3' 
+			SELECT @result = SUM(HoursInMonth) 
+			FROM HoursInMonths 
+			WHERE [Year] = @year AND [Month] IN (4, 5, 6)
+	ELSE IF @quarter = 'Q4' 
+			SELECT @result = SUM(HoursInMonth) 
+			FROM HoursInMonths 
+			WHERE [Year] = @year AND [Month] IN (7, 8, 9)
+	ELSE
+		SELECT @result = SUM(HoursInMonth) 
+			FROM HoursInMonths
+			WHERE ([Year] = @year - 1 AND [Month] IN (10, 11, 12))
+					OR ([Year] = @year AND [Month] NOT IN (10, 11, 12))
+							
+	-- Return the result of the function
+	RETURN ISNULL(@result, -1)
+
+END
+
+GO
