820b9275deb82651ba942901fdc3688ac32d44ef
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:15 +0000

    Implemented the Audit Interceptor which can now audit CUD funcationality for any object transparently. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4846. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6820

--- FSNEP.Tests/Auditing/AuditInterceptorTests.cs
+++ FSNEP.Tests/Auditing/AuditInterceptorTests.cs
@@ -22,6 +22,19 @@ namespace FSNEP.Tests.Auditing
         }
 
         [TestMethod]
+        public void AuditObjectModificationSouldNotSaveAuditEntity()
+        {
+            AuditInterceptor.UserAuth.Expect(a => a.CurrentUserName).Return("currentUser");
+
+            var auditRepository = MockRepository.GenerateStub<IRepository<Audit>>();
+            AuditInterceptor.Repository.Expect(a => a.OfType<Audit>()).Return(auditRepository);
+
+            AuditInterceptor.AuditObjectModification(new Audit(), null, AuditActionType.Update);
+
+            auditRepository.AssertWasNotCalled(a => a.EnsurePersistent(Arg<Audit>.Is.Anything));
+        }
+
+        [TestMethod]
         public void AuditObjectModificationSavesTheCurrentUser()
         {
             Audit audit = null;
--- FSNEP/AuditInterceptor.cs
+++ FSNEP/AuditInterceptor.cs
@@ -1,5 +1,4 @@
 using System;
-using CAESArch.BLL;
 using CAESArch.Core.DataInterfaces;
 using FSNEP.BLL.Interfaces;
 using NHibernate;
@@ -23,11 +22,29 @@ namespace FSNEP
 
         public override void OnDelete(object entity, object id, object[] state, string[] propertyNames, NHibernate.Type.IType[] types)
         {
+            AuditObjectModification(entity, id, AuditActionType.Delete);
+
             base.OnDelete(entity, id, state, propertyNames, types);
+        }
+
+        public override bool OnSave(object entity, object id, object[] state, string[] propertyNames, NHibernate.Type.IType[] types)
+        {
+            AuditObjectModification(entity, id, AuditActionType.Create);
+
+            return base.OnSave(entity, id, state, propertyNames, types);
+        }
+
+        public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, NHibernate.Type.IType[] types)
+        {
+            AuditObjectModification(entity, id, AuditActionType.Update);
+
+            return base.OnFlushDirty(entity, id, currentState, previousState, propertyNames, types);
         }
 
         public void AuditObjectModification(object entity, object id, AuditActionType auditActionType)
         {
+            if (entity is Audit) return;
+
             var audit = new Audit
             {
                 AuditDate = DateTime.Now,
