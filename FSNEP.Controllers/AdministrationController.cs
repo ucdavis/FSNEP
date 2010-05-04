9797c20ffd88a360a04cdfb6e6fa4cc7b0bca126
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:32:32 +0000

    Created a blank new admin controller -- now i'm going to install resharper before i forget. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4649. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6701

---
+++ FSNEP.Controllers/AdministrationController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace FSNEP.Controllers
+{
+    public class AdministrationController : Controller
+    {
+
+    }
+}
