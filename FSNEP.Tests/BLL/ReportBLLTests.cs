697d5d310cb384fe8c796f18cfd7479d7ac1d76a
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:39:50 +0000

    Added reportBLL Tests, but they will need to be expanded once the ReportBLL is completed. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5677. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7220

---
+++ FSNEP.Tests/BLL/ReportBLLTests.cs
@@ -0,0 +1,41 @@
+﻿using FSNEP.BLL.Dev;
+using FSNEP.Core.Abstractions;
+using FSNEP.Tests.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FSNEP.Tests.BLL
+{
+    [TestClass]
+    public class ReportBLLTests
+    {
+        private IReportBLL _reportBLL;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _reportBLL = new ReportBLL();
+        }
+
+        [TestMethod]
+        public void GenerateIndividualTimeRecordReportGeneratesReport()
+        {
+            var timeRecord = CreateValidEntities.TimeRecord(null);
+
+            var result = _reportBLL.GenerateIndividualTimeRecordReport(timeRecord, ReportType.PDF);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ReportResult));
+            Assert.AreEqual("contentType", result.ContentType);
+            Assert.AreEqual(new byte[1].ToString(), result.ReportContent.ToString());
+        }
+
+        [TestMethod]
+        public void GenerateCostShareReturnsReportResult()
+        {
+            var result = _reportBLL.GenerateCostShare(CreateValidEntities.Project(null), 2009, ReportType.Excel);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ReportResult));
+            Assert.AreEqual("contentType", result.ContentType);
+            Assert.AreEqual(new byte[1].ToString(), result.ReportContent.ToString());
+        }
+    }
+}
