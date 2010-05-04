cfa63dbd781a5152f95669850268639b2f365fb4
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:35:34 +0000

    Extend the ModelState to assert that specific errors exist and that extra errors do not. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5024. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6920

--- FSNEP.Tests/Core/Extensions/AssertExtensions.cs
+++ FSNEP.Tests/Core/Extensions/AssertExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FSNEP.Tests.Core.Extensions
@@ -15,5 +16,42 @@ namespace FSNEP.Tests.Core.Extensions
             Assert.IsTrue(list.Contains(str), message);
         }
 
+        /// <summary>
+        /// Asserts the errors are exactly as specified.
+        /// </summary>
+        /// <param name="modelState">State of the model.</param>
+        /// <param name="errors">The errors.</param>
+        public static void AssertErrorsAre(this ModelStateDictionary modelState, params string[] errors)
+        {
+            var resultsList = new List<string>();
+
+            foreach (var result in modelState.Values)
+            {
+                foreach (var errs in result.Errors)
+                {
+                    resultsList.Add(errs.ErrorMessage);
+                }
+            }
+
+            Assert.AreEqual(resultsList.Count, errors.Length, "Number of error messages do not match");
+            //foreach (var error in errors)
+            //{
+            //    Assert.IsTrue(resultsList.Contains(error), "Expected error \"" + error + "\" not found");
+            //}
+            //newUserModel.ViewData.ModelState.AssertErrorsAre("FirstName: The value cannot be null.",
+            //                                                 "FirstName: The value cannot be null.");
+            //Changed it so the above cut and paste error with a unit test could not happen.
+            var errorsList = new List<string>();
+            foreach (var error  in errors)
+            {
+                errorsList.Add(error);
+            }
+
+            //Look through the errors in the modelState and check they are as expected.
+            foreach (var error in resultsList)
+            {
+                Assert.IsTrue(errorsList.Contains(error), "Expected error \"" + error + "\" not found");
+            }
+        }
     }
 }
