59c04e2164fbc32ec367baf602ab971e983fc523
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:34 +0000

    Added Repository Test for HoursInMonth (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4930. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6844

---
+++ FSNEP.Tests/Repositories/HoursInMonthRepositoryTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using CAESArch.BLL;
+using CAESArch.BLL.Repositories;
+using FSNEP.Tests.Core;
+using FSNEP.Tests.Core.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FSNEP.Core.Domain;
+using FSNEP.BLL.Impl;
+using Rhino.Mocks;
+using FSNEP.BLL.Interfaces;
+using FSNEP.Core.Abstractions;
+using CAESArch.BLL;
+using FSNEP.Tests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FSNEP.Core.Domain;
+using CAESArch.Core.DataInterfaces;
+using CAESArch.BLL.Repositories;
+
+namespace FSNEP.Tests.Repositories
+{
+    [TestClass]
+    public class HoursInMonthRepositoryTests : RepositoryTestBase
+    {
+        private readonly IRepository<HoursInMonth> _hoursInMonthRepository = new Repository<HoursInMonth>();
+
+        //Don't load any data
+        protected override void LoadData() { }
+
+        [TestMethod]
+        public void CanSaveCompleteAndValidHoursInMonth()
+        {
+            var hoursInMonth = new HoursInMonth(2009, 09) {Hours = 100};
+
+            using (var ts = new TransactionScope())
+            {
+                _hoursInMonthRepository.EnsurePersistent(hoursInMonth);
+
+                ts.CommitTransaction();
+            }
+
+            Assert.AreEqual(false, hoursInMonth.IsTransient());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void HoursInMonthDoesNotSaveWithZeroHours()
+        {
+            var hoursInMonth = new HoursInMonth(2009, 09) { Hours = 0 };
+
+            try
+            {
+                using (var ts = new TransactionScope())
+                {
+                    _hoursInMonthRepository.EnsurePersistent(hoursInMonth);
+
+                    ts.CommitTransaction();
+                }
+            }
+            catch(Exception message)
+            {
+                Assert.AreEqual("Object of type FSNEP.Core.Domain.HoursInMonth could not be persisted\n\n\r\nValidation Errors: Hours, The value must fall within the range \"1\" (Inclusive) - \"0\" (Ignore).\r\n", message.Message, "Expected Exception Not encountered");
+                throw;
+            }
+        }
+    }
+}
