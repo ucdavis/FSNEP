92368e3d02c3a796f3e3f96767cb46cefba3a075
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:41:35 +0000

    Created scripts to Update the email/username quick lookup columns from the aspnet_xxx tables (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6223. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7359

---
+++ FSNEP.SQL/Helpers/UpdateEmailAndUserNames.sql
@@ -0,0 +1,14 @@
+﻿/* Update the email/username quick lookup columns from the aspnet_xxx tables */
+update FSNEP2.dbo.Users
+set Email = (
+	select membership.Email
+	from FSNEP.dbo.aspnet_Membership membership
+	where Users.UserId = membership.UserId
+)
+
+update FSNEP2.dbo.Users
+set UserName = (
+	select u.UserName
+	from FSNEP.dbo.aspnet_Users u
+	where Users.UserId = u.UserId
+)
