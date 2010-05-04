378e250d4a6d12a57c56f144775eb0d1d43c6cdc
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:42 +0000

    Added Repository tests for ActivityCategory (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4934. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6848

---
+++ FSNEP.Tests/Repositories/ActivityCategoryRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using CAESArch.BLL;
+using CAESArch.BLL.Repositories;
+using CAESArch.Core.DataInterfaces;
+using FSNEP.Core.Domain;
+using FSNEP.Tests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FSNEP.Tests.Repositories
+{
+    [TestClass]
+    public class ActivityCategoryRepositoryTests : RepositoryTestBase
+    {
+        private readonly IRepository<ActivityCategory> _activityCategoryRepository = new Repository<ActivityCategory>();
+
+        //Don't load any data
+        protected override void LoadData() { }
+
+        [TestMethod]
+        public void CanSaveCompleteAndValidActivityCategory()
+        {
+            var activityCategory = new ActivityCategory { Name = "123456789 123456789 123456789 123456789 1234567890" };
+
+            using (var ts = new TransactionScope())
+            {
+                _activityCategoryRepository.EnsurePersistent(activityCategory);
+
+                ts.CommitTransaction();
+            }
+
+            Assert.AreEqual(false, activityCategory.IsTransient());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void ActivityCategoryDoesNotSaveWithNullName()
+        {
+            var activityCategory = new ActivityCategory { Name = null };
+
+            try
+            {
+                using (var ts = new TransactionScope())
+                {
+                    _activityCategoryRepository.EnsurePersistent(activityCategory);
+
+                    ts.CommitTransaction();
+                }
+            }
+            catch (Exception message)
+            {
+                Assert.AreEqual("Object of type FSNEP.Core.Domain.ActivityCategory could not be persisted\n\n\r\nValidation Errors: Name, The value cannot be null.\r\nName, The length of the value must fall within the range \"0\" (Ignore) - \"50\" (Inclusive).\r\n", message.Message, "Expected Exception Not encountered");
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void ActivityCategoryDoesNotSaveWithNameGreaterThan50Characters()
+        {
+            var activityCategory = new ActivityCategory { Name = "123456789 123456789 123456789 123456789 123456789 1" };
+
+            try
+            {
+                using (var ts = new TransactionScope())
+                {
+                    _activityCategoryRepository.EnsurePersistent(activityCategory);
+
+                    ts.CommitTransaction();
+                }
+            }
+            catch (Exception message)
+            {
+                Assert.AreEqual("Object of type FSNEP.Core.Domain.ActivityCategory could not be persisted\n\n\r\nValidation Errors: Name, The length of the value must fall within the range \"0\" (Ignore) - \"50\" (Inclusive).\r\n", message.Message, "Expected Exception Not encountered");
+                throw;
+            }
+        }
+    }
+}
