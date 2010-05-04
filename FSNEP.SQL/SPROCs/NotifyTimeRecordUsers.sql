d0a482899c232368a88c45a2372580ded94177bb
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:40:52 +0000

    Added an SPROC which can filter down the users to email about submitting their time records.  The criteria still needs to be defined for when to send, and i haven't yet wrote the send method (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5925. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7305

---
+++ FSNEP.SQL/SPROCs/NotifyTimeRecordUsers.sql
@@ -0,0 +1,36 @@
+﻿USE [FSNEPv2]
+
+DECLARE @dayInt int
+DECLARE @nowMinusOneMonth datetime
+DECLARE @previousMonthInt int
+DECLARE @previousYearInt int
+
+SET @dayInt = (SELECT DATEPART(DAY, GETDATE()))
+
+SET @nowMinusOneMonth = (SELECT DATEADD(month, -1, GETDATE()) as PrevMonth)
+SET @previousMonthInt = (SELECT DATEPART(month, @nowMinusOneMonth))
+SET @previousYearInt = (SELECT DATEPART(year, @nowMinusOneMonth))
+
+SELECT @previousMonthInt, @previousYearInt
+
+--Get those with submitted time records for the previous month
+SELECT     members.Email, Users.FirstName + ' ' + Users.LastName AS FullName, sup.Email AS SupervisorEmail
+FROM         Records AS Rec INNER JOIN
+                      Users ON Users.UserId = Rec.UserId INNER JOIN
+                      aspnet_Membership AS members ON members.UserId = Rec.UserId INNER JOIN
+                      aspnet_Membership AS sup ON sup.UserId = Users.SupervisorID INNER JOIN
+                      Status ON Status.ID = Rec.StatusID INNER JOIN
+                      TimeRecords ON Rec.ID = TimeRecords.ID
+WHERE     (Rec.Month = @previousMonthInt) AND (Rec.Year = @previousYearInt) AND (Status.Name = 'Approved' OR
+                      Status.Name = 'PendingReview')
+
+--Get all of the FTE TimeSheet Users who should have submitted a time record
+SELECT     aspnet_Membership.Email, Users.FirstName, Users.LastName, sup.Email
+FROM         aspnet_Membership INNER JOIN
+                      Users ON aspnet_Membership.UserId = Users.UserId INNER JOIN
+                      aspnet_UsersInRoles ON aspnet_Membership.UserId = aspnet_UsersInRoles.UserId INNER JOIN
+                      aspnet_Membership AS sup ON sup.UserId = Users.SupervisorID INNER JOIN
+                      aspnet_Roles ON aspnet_UsersInRoles.RoleId = aspnet_Roles.RoleId 
+WHERE     (aspnet_Roles.RoleName = N'Timesheet User') AND (Users.IsActive = 1)
+
+--Send Emails on some date (TBD) to those who should have submitted a time record and didn't 
