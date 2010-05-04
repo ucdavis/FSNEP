1821cd264bbe667a0e5af47fd018c7c591a45cf5
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:34:56 +0000

    Added self validation to the hoursinmonth class to check its id YearMonthComposite for date range issues. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4956. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C6868

--- FSNEP.Core/Domain/HoursInMonth.cs
+++ FSNEP.Core/Domain/HoursInMonth.cs
@@ -1,9 +1,11 @@
 ﻿using CAESArch.Core.Domain;
 using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
 using System;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
 
 namespace FSNEP.Core.Domain
 {
+    [HasSelfValidation]
     public class HoursInMonth : DomainObject<HoursInMonth, YearMonthComposite>
     {
         [RangeValidator(1, RangeBoundaryType.Inclusive, 0, RangeBoundaryType.Ignore)] //greater than 0
@@ -17,6 +19,19 @@ namespace FSNEP.Core.Domain
         public HoursInMonth(int year, int month)
         {
             id = new YearMonthComposite(month, year);
+        }
+
+        [SelfValidation]
+        public void Validate(ValidationResults results)
+        {
+            try
+            {
+                new DateTime(id.Year, id.Month, 1); //Try to create a new datetime using the id date values
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                results.AddResult(new ValidationResult("The year and month entered are not valid", this, "id", null,null));
+            }
         }
 
         public override string ToString()
