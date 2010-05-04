3aa87f03794293246a0177874e337e7ee1c1423d
SU for Alan Lai <srkirkland@gmail.com>
2010-05-04 21:37:15 +0000

    Adding unit tests for TimeRecordController (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5198. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/FSNEP/Trunk/FSNEP-MVC;C7039

---
+++ FSNEP.Tests/Controllers/TimeRecordControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using FSNEP.BLL.Impl;
+using FSNEP.BLL.Interfaces;
+using FSNEP.Controllers;
+using FSNEP.Core.Calendar;
+using FSNEP.Tests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Rhino.Mocks;
+using FSNEP.Core.Domain;
+using FSNEP.Core.Abstractions;
+using System.Web.Security;
+using FSNEP.Tests.Core.Extensions;
+using UCDArch.Testing;
+using Rhino.Mocks.Interfaces;
+
+namespace FSNEP.Tests.Controllers
+{
+    [TestClass]
+    public class TimeRecordControllerTests : Core.ControllerTestBase<TimeRecordController>
+    {
+        protected override void SetupController()
+        {
+            var timeRecordBll = MockRepository.GenerateStub<ITimeRecordBLL>();
+            var userBll = MockRepository.GenerateStub<IUserBLL>();
+            var timeRecordCalendarGenerator = MockRepository.GenerateStub<ITimeRecordCalendarGenerator>();
+
+            CreateController(timeRecordBll, userBll, timeRecordCalendarGenerator);
+        }
+
+        #region Routing maps
+        [TestMethod]
+        public void RoutingTimeRecordEntryMapsEntry()
+        {
+            const int id = 5;
+            "~/TimeRecord/TimeRecordEntry/5"
+                .ShouldMapTo<TimeRecordController>(a => a.TimeRecordEntry(id));
+        }
+
+        #endregion Routing maps
+    }
+}
